da33a8bc6abb66d31c1c0183a36e37eeb77e6afb
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-12-15 15:13:10 -0800

    refactoring

--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import {robots} from './robots';
 import 'tachyons';
+import CardList from './CardList'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div>
-    <Card id={robots[0].id} name = {robots[0].name} email={robots[0].email} username= {robots[0].username}/>
-    <Card id={robots[1].id} name = {robots[1].name} email={robots[1].email} username= {robots[1].username}/>
-    <Card id={robots[2].id} name = {robots[2].name} email={robots[2].email} username= {robots[2].username}/>
-    
+   <CardList robots={robots}></CardList>
     </div>
   </React.StrictMode>
 );
--- src/robots.js
+++ src/robots.js
@@ -17,7 +17,47 @@ export const robots = [
     username: 'bobbypropane',
     email:'bobby@propane.net'
 },
-
-
+{
+    id:4,
+    name:'MehMan',
+    username: 'mehman66',
+    email:'meh@meh.com'
+},
+{
+    id:5,
+    name:'Dora',
+    username: 'doradoradora',
+    email:'dor@explorer.com'
+},
+{
+    id:6,
+    name:'Romma',
+    username: 'romma22',
+    email:'jrrr@aol.com'
+},
+{
+    id:7,
+    name:'RuwuTutu',
+    username: 'ruwa2212',
+    email:'tuag@gmail.com'
+},
+{
+    id:8,
+    name:'Ruffis',
+    username: 'ligma2',
+    email:'ligma@propane.net'
+},
+{
+    id:9,
+    name:'Brock',
+    username: 'brockrocks',
+    email:'brock@meh.com'
+},
+{
+    id:10,
+    name:'Jojo',
+    username: 'JojosBizzare',
+    email:'jojo@explorer.com'
+}
 
 ]
