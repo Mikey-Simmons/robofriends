5a20cd297d43dac1de6def1a5b999c25916a685d
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-12-15 12:09:24 -0800

    firstBuild

---
+++ src/robots.js
@@ -0,0 +1,23 @@
+export const robots = [
+{
+    id:1,
+    name:'Joe',
+    username: 'joesmo420',
+    email:'joeysmo@smo.com'
+},
+{
+    id:2,
+    name:'Randy',
+    username: 'randyjack',
+    email:'randyjack@randyjack.org'
+},
+{
+    id:3,
+    name:'Bob',
+    username: 'bobbypropane',
+    email:'bobby@propane.net'
+},
+
+
+
+]
