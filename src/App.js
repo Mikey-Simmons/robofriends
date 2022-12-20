f9f7b5eacdec33702072bf6e0d118ed2c9ad6e12
Michael Simmons <64806379+Mikey-Simmons@users.noreply.github.com>
2022-12-20 15:29:06 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import {robots} from './robots';
+
 import SearchBox from './SearchBox';
 import './index.css';
 
@@ -10,9 +10,15 @@ class App extends Component{
     constructor(){
             super()
             this.state ={
-            robots: robots,
+            robots: [],
             searchfield: ''
              }
+    }
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=>  response.json())
+        .then(users=> this.setState({robots : users}));
+        
     }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
